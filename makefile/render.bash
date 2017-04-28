287e2b66dde1af0730cd16328e18981d5df11eb9
avallecam <avallecam@gmail.com>
2017-04-28 12:06:38 -0500

    Add cover to render.sh + No imageJ

--- makefile/render.bash
+++ makefile/render.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cp current/resume_VALLE.pdf release-pdf/CV_ValleCampos-Andree.pdf
+cp current/cover_VALLE.pdf release-pdf/CoverLetter_ValleCampos-Andree.pdf
 cp current/resume_VALLE.pdf release-pdf/CV_ValleCampos-Andree-ENG.pdf
 cp spanish/resume_VALLE-ESP.pdf release-pdf/CV_ValleCampos-Andree-ESP.pdf
 mupdf release-pdf/CV_ValleCampos-Andree.pdf
+mupdf release-pdf/CoverLetter_ValleCampos-Andree.pdf
 mupdf release-pdf/CV_ValleCampos-Andree-ESP.pdf
